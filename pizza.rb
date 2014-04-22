e124eba9e7fdbb0408d7d88b551d252812ec2cd8
Apprentice <apprentice@dbc-chicago-4.local>
2014-04-22 13:32:50 -0500

    finished with code 2

--- pizza.rb
+++ pizza.rb
@@ -1,7 +1,19 @@
 class Pizza
-  attr_reader :name
+  attr_reader :name, :required_bake_time
 
   def initialize(name)
     @name = name
+    @required_bake_time = 1000
   end
+
+end
+
+class Topping
+  attr_reader :type, :qty
+
+  def initialize(type, qty)
+    @type = type
+    @qty = qty
+  end
+
 end
