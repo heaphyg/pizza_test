90f19c86e036dafc89bd9e4b978d0d248f09fc0e
Apprentice <apprentice@dbc-chicago-001.local>
2014-04-22 11:26:58 -0500

    New Tops Spec

--- test_pizza.rb
+++ test_pizza.rb
@@ -2,6 +2,18 @@ require 'rspec'
 require_relative 'pizza'
 
 #spec
+describe Topping do
+  let(:topping) { Topping.new("pepperoni", 200) }
+  context "#initialize" do
+    it "creates a Topping object" do
+      expect(topping).to be_an_instance_of Topping
+    end
+    it "requires two paramters" do
+      expect {Topping.new}.to raise_error(ArgumentError)
+    end
+  end
+end
+
 describe Pizza do
   let(:pizza) { Pizza.new("Ham") }
 
@@ -18,6 +30,12 @@ describe Pizza do
   context "#name" do
     it "should return a string" do
       expect(pizza.name).to be_a String
+    end
+  end
+
+  context ".required_bake_time" do
+    it "returns the correct total bake time" do
+      expect(pizza.required_bake_time).to eql(1000)
     end
   end
 
